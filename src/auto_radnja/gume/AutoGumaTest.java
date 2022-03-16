8abe69915ef424b976cb46d689c8df59e1a41140
ilijaradojkovic <radojkovicika@gmail.com>
2022-03-16 19:39:09 +0300

    Testovi za klasu AutoGuma su uradjeni

--- src/auto_radnja/gume/AutoGumaTest.java
+++ src/auto_radnja/gume/AutoGumaTest.java
@@ -5,55 +5,166 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class AutoGumaTest {
 
+	AutoGuma a;
 	@BeforeEach
 	void setUp() throws Exception {
+		a=new AutoGuma();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		a=null;
 	}
 
 	@Test
 	void testAutoGuma() {
-		fail("Not yet implemented");
+		
 	}
 
-	@Test
-	void testAutoGumaStringIntIntInt() {
-		fail("Not yet implemented");
-	}
-
+	
 	@Test
 	void testSetMarkaModel() {
-		fail("Not yet implemented");
+		a.setMarkaModel("Marka");
+		assertEquals("Marka", a.getMarkaModel());
 	}
 
 	@Test
+	void testSetMarkaModel_Null() {
+	
+		assertThrows(NullPointerException.class, ()->	a.setMarkaModel(null));
+	}
+
+
+	@Test
+	void testSetMarkaModel_DuzinaFails() {
+	
+		assertThrows(IllegalArgumentException.class, ()->	a.setMarkaModel("f"));
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"15", "19", "20"
+	})
+	
 	void testSetPrecnik() {
-		fail("Not yet implemented");
+		a.setPrecnik(15);
+		assertEquals(15, a.getPrecnik());
 	}
 
-	@Test
+	@ParameterizedTest
+	@CsvSource({
+		"11", "2", "8"
+	})
+	void testSetPrecnik_DonjiOpsegFails() {
+		assertThrows(IllegalArgumentException.class, ()->	a.setPrecnik(23));
+		
+
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"25", "40", "60"
+	})
+	void testSetPrecnik_GornjiOpsegFails() {
+		assertThrows(IllegalArgumentException.class, ()->	a.setPrecnik(23));
+
+
+	}
+	@ParameterizedTest
+	@CsvSource({
+		"150", "190", "250"
+	})
 	void testSetSirina() {
-		fail("Not yet implemented");
+		a.setSirina(200);
+		assertEquals(200, a.getSirina());
 	}
 
-	@Test
-	void testSetVisina() {
-		fail("Not yet implemented");
+	@ParameterizedTest
+	@CsvSource({
+		"50", "45", "60"
+	})
+	void testSetSirina_DonjiOpsegFail() {
+		assertThrows(IllegalArgumentException.class, ()->	a.setSirina(134));
+		
+	}
+
+
+	@ParameterizedTest
+	@CsvSource({
+		"400", "500", "390"
+	})
+	void testSetSirina_GornjiOpsegFail() {
+		assertThrows(IllegalArgumentException.class, ()->	a.setSirina(134));
+		
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"50", "45", "60"
+	})
+	
+	void testSetVisina(int visina) {
+		a.setVisina(visina);
+		assertEquals(visina,a.getVisina());
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"100", "200", "130"
+	})
+
+	void testSetVisina_GornjiOpsegFail(int visina) {
+		
+		assertThrows(IllegalArgumentException.class, ()->	a.setVisina(visina));
+	}
+	
+
+	@ParameterizedTest
+	@CsvSource({
+		"20", "13", "9"
+	})
+	void testSetVisina_DonjiOpsegFail(int visina) {
+		
+		assertThrows(IllegalArgumentException.class, ()->	a.setVisina(visina));
 	}
 
 	@Test
 	void testToString() {
-		fail("Not yet implemented");
+AutoGuma a1 = new AutoGuma("Marka",15,200,70);
+	
+		
+		
+		
+		
+		String s = a1.toString();
+		assertTrue(s.contains("Marka"),"toString ne sadrzi marku");
+		assertTrue(s.contains("15"),"toString ne sadrzi precnik");
+		assertTrue(s.contains("200"),"toString ne sadrzi sirinu");
+		assertTrue(s.contains("70"),"toString ne sadrzi visinu");
 	}
 
-	@Test
-	void testEqualsObject() {
-		fail("Not yet implemented");
+	@ParameterizedTest
+	@CsvSource({
+		
+		"Marka, 15, 200, 70, Marka, 15, 200, 70, true ",
+		"Marka, 15, 200, 70, Markaa, 15, 200, 70, false ",
+		"Marka, 17, 200, 70, Marka, 15, 200, 80, false ",
+		"Marka, 15, 201, 70, Marka, 15, 200, 80, false ",
+		"Marka, 15, 200, 70, Marka, 15, 200, 80, false ",
+	})
+	void testEqualsObject(String markaPrvog,int precnikPrvog,int sirinaPrvog,int visinaPrvog,String markaDrugog,int precnikDrugog,int sirinaDrugog,int visinaDrugog,boolean result) {
+		AutoGuma a1 = new AutoGuma(markaPrvog,precnikPrvog,sirinaPrvog,visinaPrvog);
+		a.setMarkaModel(markaDrugog);
+		a.setPrecnik(precnikDrugog);
+		a.setSirina(sirinaDrugog);
+		a.setVisina(visinaDrugog);
+	
+		assertEquals(result, a.equals(a1));
 	}
 
 }
