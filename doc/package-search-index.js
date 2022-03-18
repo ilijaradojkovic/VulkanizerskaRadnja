474a0c1fea72bacebcc8aeeb929b93ed72ff5fe9
ilijaradojkovic <radojkovicika@gmail.com>
2022-03-18 15:03:03 +0300

    doc folder izbacen

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"auto_radnja.gume"}];updateSearchResults();
--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"auto_radnja.gume","l":"AutoGuma"},{"p":"auto_radnja","l":"Radnja"}];updateSearchResults();
